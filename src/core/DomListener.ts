428e8932175db99426da93a718f2d75d4d043700
IvanPostu <ipostu20000127@gmail.com>
2020-05-25 13:41:12 -0400

    Added simple event listeners.

--- src/core/DomListener.ts
+++ src/core/DomListener.ts
@@ -1,14 +1,45 @@
-export class DomListener {
-  private $root: Node | string
+import { $ } from '@/core/dom'
 
+export class DomListener {
+  private $root: ReturnType<typeof $>
+  private listeners: Array<string>
   /**
    *
    */
-  constructor($root: Node | string) {
+  constructor($root: ReturnType<typeof $>, listeners = []) {
     if (!$root) {
       throw Error('No $root provided from DomListener')
     }
 
     this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const methodName = getMethodName(listener)
+
+      if (!this[methodName]) {
+        throw new Error(
+          `Method ${methodName} is not implemented in ${this['name'] || ''} Component.`,
+        )
+      }
+
+      this.$root.on(listener, this[methodName])
+    })
+  }
+
+  removeDOMListeners() {}
+}
+
+/**
+ *
+ * @param str
+ *
+ * convert listener name to event method name
+ * ex: 'input' => 'onInput'
+ *
+ */
+function getMethodName(str: string): string {
+  return 'on' + str.charAt(0).toUpperCase() + str.slice(1)
 }
